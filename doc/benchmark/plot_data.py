d8165465c201c579a4e6fa26d351c4c0f3dda52d
WJFeng <wj.feng@tum.de>
2023-06-30 21:48:51 +0200

    Benchmark pic

--- doc/benchmark/plot_data.py
+++ doc/benchmark/plot_data.py
@@ -1,29 +1,49 @@
 import matplotlib.pyplot as plt
 
 threads = []
-times = []
+times_malloc = []
+times_concurrentMalloc = []
+times_concurrentMallocRadixTree = []
 
 # 读取文件并解析线程数和执行时间
 with open("dataMalloc.txt", "r") as file:
     for line in file:
-        thread, time = line.strip().split(":")
+        thread, time_malloc = line.strip().split(":")
         threads.append(int(thread))
-        times.append(int(time))
+        times_malloc.append(int(time_malloc))
+
+with open("dataConcurrentMalloc.txt", "r") as file:
+    for line in file:
+        _, time_concurrentMalloc = line.strip().split(":")
+        times_concurrentMalloc.append(int(time_concurrentMalloc))
+
+with open("dataConcurrentMallocRadixTree.txt", "r") as file:
+    for line in file:
+        _, time_concurrentMallocRadixTree = line.strip().split(":")
+        times_concurrentMallocRadixTree.append(int(time_concurrentMallocRadixTree))
 
 # 去重
 threads_unique = list(set(threads))
 threads_unique.sort()
 
-# 为每个线程数计算平均执行时间
-avg_times = []
-for thread in threads_unique:
-    thread_times = [time for t, time in zip(threads, times) if t == thread]
-    avg_time = sum(thread_times) / len(thread_times)
-    avg_times.append(avg_time)
 
 # 绘制折线图
-plt.plot(threads_unique, avg_times)
+plt.figure()
+plt.plot(threads_unique, times_malloc, marker='*', label='malloc', color='red');
+plt.plot(threads_unique, times_concurrentMalloc, marker='^', label='ConcurrentMalloc', color='blue')
+plt.legend()
 plt.xlabel("Threads")
 plt.ylabel("Time (ms)")
 plt.title("Benchmark Results")
 plt.savefig("BenchmarkBeforeRadix.png")
+#plt.show()
+
+plt.figure()
+plt.plot(threads_unique, times_concurrentMalloc, marker='^', label='ConcurrentMalloc', color='blue');
+plt.plot(threads_unique, times_concurrentMallocRadixTree, marker='d', label='ConcurrentMallocRadixTree', color='green')
+plt.legend()
+plt.xlabel("Threads")
+plt.ylabel("Time (ms)")
+plt.title("Benchmark Results")
+plt.savefig("BenchmarkAfterRadix.png")
+#plt.show()
